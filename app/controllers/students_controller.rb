a0f6e4ec45e85069e4c62ce6126d490d976a8fc3
MORRIS <morris24kariuki@gmail.com>
2023-03-14 13:43:48 +0300

    added string query action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,17 @@
 class StudentsController < ApplicationController
 
+  # def index
+  #   students = Student.all
+  #   render json: students
+  # end
   def index
-    students = Student.all
-    render json: students
+    if params[:name]
+      students=Student.where("last_name LIKE ? OR first_name LIKE ?",params[:name],params[:name])
+      render json: students
+
+    else 
+      render json: students=Student.all 
+    end
   end
 
   def show
